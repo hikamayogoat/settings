c82797d9ff2b3fb7411213355dee0916e8dddfc0
田代光 <hikamayo.dw@gmail.com>
2025-01-23 21:06:09 +0900

    add comment

--- commands/git_settings.sh
+++ commands/git_settings.sh
@@ -1,3 +1,6 @@
+# セキュリティ的な意味でも GitHub の推奨する方法を信じてみると安心感があるんじゃないかと思うのでよしなに
+# https://docs.github.com/ja/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
+
 # SSHキーを作成し、GitHub に公開鍵を登録しておく
 ssh-keygen -t ed25519 -C "${EMAIL}" -f ~/.ssh/github
 
