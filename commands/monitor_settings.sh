13f8e940d3eb721f059a17b32e6ac6aba9755073
田代光 <hikamayo.dw@gmail.com>
2025-03-31 22:19:35 +0900

    ディスプレイ設定

---
+++ commands/monitor_settings.sh
@@ -0,0 +1,11 @@
+# install displayplacer
+# current モニターの設定を書き出し、いつでもコマンド実行でその状態になれる
+
+# install
+brew install displayplacer
+
+# get command
+displayplacer list
+
+# set Raycast script
+Raycast settings > Create Script Command を有効化し、作成しておくとよい
